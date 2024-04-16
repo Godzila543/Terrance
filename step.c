5bee2a24d8e46ed5f1ca53071643733b9ba50c59
Colin Brown <colin2003az@gmail.com>
2024-04-15 17:09:19 -0700

    pigpoi error

--- step.c
+++ step.c
@@ -22,76 +22,81 @@ void setup(int *periods)
     periods[3] = 10.0E6 / (rpm_4 * 200.0 / 60.0);
 
     // Initialize GPIOs
-    gpioInitialise();
-
-    // Set GPIOs as outputs
-    gpioSetMode(pin1, PI_OUTPUT); // Set GPIO18 as output.
-    gpioSetMode(pin2, PI_OUTPUT); // Set GPIO18 as output.
-    gpioSetMode(pin3, PI_OUTPUT); // Set GPIO18 as output.
-    gpioSetMode(pin4, PI_OUTPUT); // Set GPIO18 as output.
-}
-
-// test
-void main(void)
-{
-    // Get periods
-    int periods[4];
-    setup(periods);
-
-    // Initialize the previous step times
-    uint32_t previous_step[4];
-    for (unsigned char i = 0; i < 4; i++)
+    if (gpioInitialise() < 0)
     {
-        previous_step[i] = 0;
+        while (1)
+        {
+            fprintf(stderr, "pigpio initialization failed\n");
+        };
+
+        // Set GPIOs as outputs
+        gpioSetMode(pin1, PI_OUTPUT); // Set GPIO18 as output.
+        gpioSetMode(pin2, PI_OUTPUT); // Set GPIO18 as output.
+        gpioSetMode(pin3, PI_OUTPUT); // Set GPIO18 as output.
+        gpioSetMode(pin4, PI_OUTPUT); // Set GPIO18 as output.
     }
 
-    while (1)
+    // test
+    void main(void)
     {
-        // Get current time
-        uint32_t current_time = gpioTick();
+        // Get periods
+        int periods[4];
+        setup(periods);
 
-        // Motor 1
-        if (current_time - previous_step[0] > periods[0])
+        // Initialize the previous step times
+        uint32_t previous_step[4];
+        for (unsigned char i = 0; i < 4; i++)
         {
-            // Step motor 1
-            gpioWrite(pin1, 1);
-            previous_step[0] = current_time;
+            previous_step[i] = 0;
         }
 
-        // Motor 2
-        if (current_time - previous_step[1] > periods[1])
+        while (1)
         {
-            // Step motor 2
-            gpioWrite(pin2, 1);
-            previous_step[1] = current_time;
+            // Get current time
+            uint32_t current_time = gpioTick();
+
+            // Motor 1
+            if (current_time - previous_step[0] > periods[0])
+            {
+                // Step motor 1
+                gpioWrite(pin1, 1);
+                previous_step[0] = current_time;
+            }
+
+            // Motor 2
+            if (current_time - previous_step[1] > periods[1])
+            {
+                // Step motor 2
+                gpioWrite(pin2, 1);
+                previous_step[1] = current_time;
+            }
+
+            // Motor 3
+            if (current_time - previous_step[2] > periods[2])
+            {
+                // Step motor 3
+                gpioWrite(pin3, 1);
+                previous_step[2] = current_time;
+            }
+
+            // Motor 4
+            if (current_time - previous_step[3] > periods[3])
+            {
+                // Step motor 4
+                gpioWrite(pin4, 1);
+                previous_step[3] = current_time;
+            }
+
+            // Stay high for 400 us
+            gpioDelay(400);
+
+            // Unpulse
+            gpioWrite(pin1, 0);
+            gpioWrite(pin2, 0);
+            gpioWrite(pin3, 0);
+            gpioWrite(pin4, 0);
+
+            // Stay low for 400 us
+            gpioDelay(400);
         }
-
-        // Motor 3
-        if (current_time - previous_step[2] > periods[2])
-        {
-            // Step motor 3
-            gpioWrite(pin3, 1);
-            previous_step[2] = current_time;
-        }
-
-        // Motor 4
-        if (current_time - previous_step[3] > periods[3])
-        {
-            // Step motor 4
-            gpioWrite(pin4, 1);
-            previous_step[3] = current_time;
-        }
-
-        // Stay high for 400 us
-        gpioDelay(400);
-
-        // Unpulse
-        gpioWrite(pin1, 0);
-        gpioWrite(pin2, 0);
-        gpioWrite(pin3, 0);
-        gpioWrite(pin4, 0);
-
-        // Stay low for 400 us
-        gpioDelay(400);
     }
-}
