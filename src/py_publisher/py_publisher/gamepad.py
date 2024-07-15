1ae90a4ebb8ad8cb831020cd27c0711518a58a99
nickyblanch <nickyblanch@email.arizona.edu>
2024-07-15 16:46:32 -0700

    idk nothing

--- src/py_publisher/py_publisher/gamepad.py
+++ src/py_publisher/py_publisher/gamepad.py
@@ -64,6 +64,29 @@ class GamepadNode(Node):
                 self.get_logger().warn("Connection refused, retrying in 1 second")
                 rclpy.spin_once(self, timeout_sec=1)
 
+    def timer_callback_old(self):
+        # Receive data
+        try:
+            data = self.sock.recv(1024)
+            if not data:  # connection closed
+                self.get_logger().error("Connection closed, reconnecting")
+                self.sock.close()
+                self.connect_to_server()
+            else:
+                self.buffer += data.decode("utf-8")
+                while "\n" in self.buffer:
+                    line, self.buffer = self.buffer.split("\n", 1)
+                    # Decode and print the data
+                    gamepad_data = json.loads(line)
+                    self.controller.update(gamepad_data)
+                    msg = String()
+                    msg.data = json.dumps(self.controller.state)
+                    self.publisher_.publish(msg)
+        except socket.error as e:
+            self.get_logger().error(f"Socket error: {e}, reconnecting")
+            self.sock.close()
+            self.connect_to_server()
+
     def timer_callback(self):
         # Receive data
         try:
